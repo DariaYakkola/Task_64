a4fac7fff99a0eada2f2fb4405d7a74c8bb2e99a
Daria Yakkola <yakkolabest@gmail.com>
2023-03-16 00:35:47 +0300

    Задача 64

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m >= n)
+{
+  Console.WriteLine("Натуральных чисел от M до N не существует! ");
+  return;
+}
+/// <summary>
+/// Метод, который печатает все натуральные числа в промежутке
+/// </summary>
+/// <param name="start">Цифра от которой идет перечесление</param>
+/// <param name="end">Цифра до которой идет перечисление</param>
+/// <returns></returns>
+string PrintNumbers (int start, int end)
+{
+  if (start == end) return start.ToString();
+  return (start + "," + PrintNumbers(start + 1, end));
+}
+Console.WriteLine(PrintNumbers(m, n));
