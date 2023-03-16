6cf5da3aac4a0f5888e59e4a50872efbd52ac2ef
Daria Yakkola <yakkolabest@gmail.com>
2023-03-16 20:38:10 +0300

    Добавила условие, которое убирает отрицательные числа

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (m >= n)
+if (m >= n | n <= 1)
 {
   Console.WriteLine("Натуральных чисел от M до N не существует! ");
   return;
@@ -15,6 +15,10 @@ if (m >= n)
 /// <returns></returns>
 string PrintNumbers (int start, int end)
 {
+  if (start <= 0)
+  {
+    start = 1;
+  }
   if (start == end) return start.ToString();
   return (start + "," + PrintNumbers(start + 1, end));
 }
